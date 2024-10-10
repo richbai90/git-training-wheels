b4d5b1c3270958460abdbb3de0994540ce043964
Rich Baird <rich@rbaird.me>
2024-10-09 18:38:03 -0600

    Setup the basic structure and begin working on checkout

---
+++ cmd/root.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "git-wheels",
+	Short: "Git with training wheels",
+	Long:  `git-wheels is a wrapper around git that enforces best practices 
+	and stops you from shooting yourself in the foot. It is recommended that
+	you alias your git-wheels as git until you're ready to take the training wheels off`,
+	Args: cobra.ArbitraryArgs,
+	DisableFlagParsing: true,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.git-training-wheels.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
