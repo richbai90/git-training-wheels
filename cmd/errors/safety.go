a6b6048dcdc72d94a027aa0c17ffca22f16b3303
Rich Baird <rich@rbaird.me>
2024-10-10 19:49:40 -0600

    Setup Error Code Scheme

--- cmd/errors/safety.go
+++ cmd/errors/safety.go
@@ -1,21 +1,42 @@
 package errors
 
+type Command uint8
+type ErrorCause uint8
+
+const (
+	commandMask uint8 = 0xF0 // 11110000
+	causeMask uint8 = 0xF1 // 00001111
+)
+
+const (
+	CheckoutCommand Command = 0x10
+	CloneCommand Command = 0x20
+	CommitCommand Command = 0x30
+	ResetCommand Command = 0x40
+	RemoveCommand Command = 0x50
+	UnusedCommand Command = 0x60
+)
+
+const (
+	CheckoutErrorUnclean ErrorCause = 0x01
+	CheckoutErrorReset ErrorCause = 0x02
+)
+
 // SafetyError represents an error that occurs during safety checks in Git operations.
 // It includes information about the error message, the command being executed,
 // and an error code for more specific error handling.
 type SafetyError struct {
 	// Message is the human-readable description of the error
 	message string
-	// Command is the Git command that was being executed when the error occurred
-	command string
 	// Code is the numeric identifier for the specific error type
-	code int
+	code uint8
 }
 
 type IntSafetyErr interface {
 	error
-	Command() string
-	Code() int
+	Command() Command
+	Cause() ErrorCause
+	Code() uint8
 }
 
 var _ IntSafetyErr = (*SafetyError)(nil)
@@ -24,18 +45,21 @@ func (se *SafetyError) Error() string {
 	return se.message
 }
 
-func (se *SafetyError) Code() int {
+func (se *SafetyError) Code() uint8 {
 	return se.code
 }
 
-func (se *SafetyError) Command() string {
-	return se.command
+func (se *SafetyError) Command() Command {
+	return Command(se.Code() & commandMask)
 }
 
-func NewSafetyError(message string, code int, command string) IntSafetyErr {
+func (se *SafetyError) Cause() ErrorCause {
+	return ErrorCause(se.Code() & causeMask)
+}
+
+func NewSafetyError(message string, code uint8) IntSafetyErr {
 	return &SafetyError {
 		message: message,
 		code: code,
-		command: command,
 	}
 }
